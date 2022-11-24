903ab9947874527fcd03095d888c3250b7ce4e73
Magomedcoder <info@magomedcoder.ru>
2022-11-24 23:27:11 +0300

    Added attribute listen

---
+++ src/Event/Attributes/Listen.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Init\Event\Attributes;
+
+use Attribute;
+
+/**
+ * Class Listen
+ * @package PCore\Init\Event\Attributes
+ * @github https://github.com/pcore-framework/init
+ */
+#[Attribute(Attribute::TARGET_CLASS)]
+class Listen
+{
+}
